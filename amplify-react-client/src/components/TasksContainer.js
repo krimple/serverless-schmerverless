eca6d925018b81189c11acc054879645071188da
Ken Rimple <krimple@chariotsolutions.com>
2020-04-15 11:07:37 -0400

    WIP front-end fixes

--- amplify-react-client/src/components/TasksContainer.js
+++ amplify-react-client/src/components/TasksContainer.js
@@ -15,58 +15,32 @@ const TasksContainer = ({api}) => {
         getToken();
     });
 
+    const taskTRs = tasks.map(t => (
+        <tr>
+            <td>{ t['taskId']['S'] }</td>
+            <td>{ t['description']['S']}</td>
+            <td>{ t['priority']['N']}</td>
+            <td>{ t['dueDate']['S']}</td>
+            <td>{ t['completed']['BOOL']}</td>
+        </tr>
+
+    ));
+
     return (
         <>
-            <div>
-              <button onClick={ () => {
-                  async function loadTasks() {
-                    const loadedTasks = await API.get('taskManagerSam', '/tasks', {
-                        headers: { Authorization: bearerToken }
-                    });
-                    console.dir(loadedTasks);
-                    setTasks(loadedTasks);
-                  }
-                  loadTasks();
-              }}>Load tasks SAM...</button>
-            </div>
-            <div>
-                <button onClick={ () => {
-                    async function loadTasks() {
-                        const loadedTasks = await API.get('taskManagerServerless', '/tasks', {
-                            headers: { Authorization: bearerToken }
-                        });
-                        console.dir(loadedTasks);
-                        setTasks(loadedTasks);
-                    }
-                    loadTasks();
-                }}>Load tasks Serverless...</button>
-            </div>
-            <div>
-                <button onClick={ () => {
+           <div>
+               <h3>Load Tasks</h3>
+                <button className="mui-btn mui-btn--primary" onClick={ () => {
                     async function loadTasks() {
                         const loadedTasks = await API.get('taskManagerNodeServerless', '/tasks', {
                             headers: { Authorization: bearerToken }
                         });
                         console.dir(loadedTasks);
-                        setTasks(loadedTasks);
+                        setTasks(loadedTasks.tasks);
                     }
                     loadTasks();
-                }}>Load tasks Serverless NodeJS...</button>
-            </div>
-            <div>
-                <button onClick={ () => {
-                    async function loadTasks() {
-                        const loadedTasks = await API.get('taskManagerArchitect', '/tasks', {
-                            headers: { Authorization: bearerToken }
-                        });
-                        console.dir(loadedTasks);
-                        setTasks(loadedTasks);
-                    }
-                    loadTasks();
-                }}>Load tasks Architect (different data model)...</button>
-            </div>
-            <div>
-                <button onClick={ () => {
+                }}>Serverless</button>
+                <button className="mui-btn mui-btn--primary" onClick={ () => {
                     async function loadTasks() {
                         const loadedTasks = await API.get('taskManagerNodeSam', '/tasks', {
                             headers: { Authorization: bearerToken }
@@ -75,9 +49,36 @@ const TasksContainer = ({api}) => {
                         setTasks(loadedTasks);
                     }
                     loadTasks();
-                }}>Load tasks NodeJS SAM</button>
+                }}>AWS SAM</button>
+                <button className="mui-btn mui-btn--primary" onClick={ () => {
+                    async function loadTasks() {
+                        const loadedTasks = await API.get('taskManagerArchitect', '/tasks', {
+                            headers: { Authorization: bearerToken }
+                        });
+                        console.dir(loadedTasks);
+                        setTasks(loadedTasks);
+                    }
+                    loadTasks();
+                }}>Architect (different data model)...</button>
             </div>
-            { tasks && (<pre>{JSON.stringify(tasks, undefined, 2)}</pre>) }
+
+           { tasks &&
+               <table className="mui-table">
+                   <thead>
+                     <tr>
+                         <th>ID</th>
+                         <th>Description</th>
+                         <th>Priority</th>
+                         <th>Due Date</th>
+                         <th>Complete</th>
+                     </tr>
+                   </thead>
+                   <tbody>
+                     { taskTRs }
+                   </tbody>
+               </table>
+
+           }
         </>
     )
 }
